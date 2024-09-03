9ff2143caa70752d02e282ff91191df5e3934949
EXCervantes <160692371+EXCervantes@users.noreply.github.com>
2024-09-03 16:21:26 -0400

    Setup map dependency and components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import LeftNav from "./components/LeftNav"
 import Navbar from "./components/Navbar"
 import PageLayout from "./components/PageLayout"
+import Map from "./components/Map"
 
 const App = () => {
   return (
     <main>
       <PageLayout/>
+      <Map />
       <LeftNav/>
       <Navbar/>
     </main>
---
+++ client/src/components/Map.jsx
@@ -0,0 +1,13 @@
+import { MapContainer, TitleLayer, useMap} from 'react-leaflet'
+
+const Map = () => {
+    return (
+        <div>
+            <h1>
+                Map Component
+            </h1>
+        </div>
+    )
+}
+
+export default Map
