875927add3140ade9bdc5a20cc66d7949b4781e0
EXCervantes <160692371+EXCervantes@users.noreply.github.com>
2024-09-03 21:50:02 -0400

    Edited Map.jsx to grab user's current location

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -1,17 +1,20 @@
-import { useState } from 'react'
-import { MapContainer, Popup, TileLayer, useMapEvents} from 'react-leaflet'
+import { useState, useEffect } from 'react'
+import { MapContainer, Popup, TileLayer, useMap} from 'react-leaflet'
 import CustomMarker from './custom-marker'
 
 const Map = () => {
     const LocationMarker = () => {
         const [position, setPosition] = useState(null);
-        const map = useMapEvents({
-          click(e) {
-            setPosition(e.latlng);
+
+        const map = useMap()
+        
+        useEffect(() => {
+          map.locate().on('locationfound', (e) => {
+            setPosition(e.latlng)
             map.flyTo(e.latlng, map.getZoom());
-          },
-        });
-      
+          }) 
+          }, [])
+            
         return position === null? null : (
           <CustomMarker position={position}>
             <Popup>You clicked here</Popup>
@@ -20,13 +23,8 @@ const Map = () => {
       }
     return (
         <div>
-            <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true}>
+            <MapContainer center={[48.856614, 2.3522219]} zoom={13} scrollWheelZoom={false}>
                 <TileLayer attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-                <CustomMarker position={[51.505, -0.09]}>
-                <Popup>
-                This is a location
-                </Popup>
-                </CustomMarker>
                 <LocationMarker/>
             </MapContainer>
         </div>
