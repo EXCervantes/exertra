b47ea55e91efcab483115ac4d88a52fb7dd68153
EXCervantes <160692371+EXCervantes@users.noreply.github.com>
2024-09-09 15:14:53 -0500

    Created Signup Form

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,30 @@
 import './lib/MockLocation.js'
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
+import LoginForm from './pages/Login.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+
+const router = createBrowserRouter({
+  {
+    path: '/',
+    element: <App/>,
+    error: <Error/>,
+    children: [
+      {
+        index: true,
+        element: <Home/>
+      }, {
+        path: '/login',
+        element: <LoginForm/>
+      }, {
+        path: '/signup'
+      }
+    ]
+  }
+})
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <RouterProvider router={router} />
 )
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,8 +1,151 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { useMutation } from "@apollo/client";
+import { ADD_USER } from "../utils/mutations";
+import { Button, Checkbox, Label, TextInput } from "flowbite-react";
+import { HiUser, HiMail, HiKey } from "react-icons/hi";
 
+import Auth from "../utils/auth";
 
+const SignupForm = () => {
+  const [formState, setFormState] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+  const [addUser, { error, data }] = useMutation(ADD_USER);
 
+  const handleChange = (event) => {
+    const { name, value } = event.target;
 
-{/* <div class="flex flex-col justify-center items-center">
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    console.log(formState);
+
+    try {
+      const { data } = await addUser({
+        variables: { ...formState },
+      });
+
+      Auth.login(data.addUser.token);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  return (
+    <div className="flex-row justify-center mb-4">
+      <div className="col-12 col-lg-10">
+        <div>
+          <h3 className="text-xl" id="login">
+            Login
+          </h3>
+          <div>
+            {data ? (
+              <p>
+                Signup successful! <Link to="/">back to the main page.</Link>
+              </p>
+            ) : (
+              <form
+                className="flex max-w-md flex-col gap-4"
+                onSubmit={handleFormSubmit}
+              >
+                <div>
+                  <div className="mb-2 block">
+                    <Label htmlFor="username" value="Your username" />
+                  </div>
+                  <TextInput
+                    id="username"
+                    type="name"
+                    rightIcon={HiUser}
+                    placeholder="UnassumingMan"
+                    value={formState.username}
+                    onChange={handleChange}
+                    required
+                    shadow
+                  />
+                </div>
+                <div>
+                  <div className="mb-2 block">
+                    <Label htmlFor="email2" value="Your email" />
+                  </div>
+                  <TextInput
+                    id="email2"
+                    type="email"
+                    rightIcon={HiMail}
+                    placeholder="email@example.com"
+                    value={formState.email}
+                    onChange={handleChange}
+                    required
+                    shadow
+                  />
+                </div>
+                <div>
+                  <div className="mb-2 block">
+                    <Label htmlFor="password2" value="Your password" />
+                  </div>
+                  <TextInput
+                    id="password2"
+                    type="password"
+                    rightIcon={HiKey}
+                    value={formState.password}
+                    onChange={handleChange}
+                    required
+                    shadow
+                  />
+                </div>
+                <div>
+                  <div className="mb-2 block">
+                    <Label htmlFor="repeat-password" value="Repeat password" />
+                  </div>
+                  <TextInput
+                    id="repeat-password"
+                    type="password"
+                    rightIcon={HiKey}
+                    value={formState.password}
+                    onChange={handleChange}
+                    required
+                    shadow
+                  />
+                </div>
+                <div className="flex items-center gap-2">
+                  <Checkbox id="agree" />
+                  <Label htmlFor="agree" className="flex">
+                    I agree with the&nbsp;
+                    <Link
+                      href="#"
+                      className="text-cyan-600 hover:underline dark:text-cyan-500"
+                    >
+                      terms and conditions
+                    </Link>
+                  </Label>
+                </div>
+                <Button type="submit">Register new account</Button>
+              </form>
+            )}
+
+            {error && (
+              <div className="my-3 p-3 bg-danger text-white">
+                {error.message}
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignupForm;
+
+{
+  /* <div class="flex flex-col justify-center items-center">
   <p class="text-xl mb-3">Signup</p>
 
   <form class="form signup-form">
@@ -22,4 +165,5 @@
       <button class="btn btn-primary" type="submit">signup</button>
     </div>
   </form>
-</div> */}
+</div> */
+}
