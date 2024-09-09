d110d9995c27074312d273375d2953352882780b
EXCervantes <160692371+EXCervantes@users.noreply.github.com>
2024-09-09 15:23:24 -0500

    Created Routers and Apollo Client wrapper in App JSX

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,51 @@
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import { Outlet } from "react-router-dom";
+
 // import LeftNav from "./components/LeftNav"
-import Navbar from "./components/Navbar"
-import Map from "./components/Map"
+import Navbar from "./components/Navbar";
+import Map from "./components/Map";
+
+// Main GraphQL API endpoint
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+
+// Middleware for handling the JWT token
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+// Execute the authLink middleware for the client prior to a request to GraphQL API
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 const App = () => {
   return (
-    <main>
-      <Navbar/>
-      <Map />
-      {/* <LeftNav/> */}
-    </main>
-  )
-}
+    <ApolloProvider client={client}>
+      <>
+        <main>
+          <Navbar />
+          <Outlet />
+          <Map />
+          {/* <LeftNav/> */}
+        </main>
+      </>
+    </ApolloProvider>
+  );
+};
 
-export default App
+export default App;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,30 +1,35 @@
-import './lib/MockLocation.js'
-import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import App from './App.jsx'
-import './index.css'
-import LoginForm from './pages/Login.jsx'
+import "./lib/MockLocation.js";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
 
+import App from "./App.jsx";
+import LoginForm from "./pages/Login.jsx";
+import SignupForm from "./pages/Signup.jsx";
+import Error from "./pages/Error.jsx";
 
-const router = createBrowserRouter({
+const router = createBrowserRouter([
   {
-    path: '/',
-    element: <App/>,
-    error: <Error/>,
+    path: "/",
+    element: <App />,
+    error: <Error />,
     children: [
       {
         index: true,
-        element: <Home/>
-      }, {
-        path: '/login',
-        element: <LoginForm/>
-      }, {
-        path: '/signup'
-      }
-    ]
-  }
-})
+        element: <Home />,
+      },
+      {
+        path: "/login",
+        element: <LoginForm />,
+      },
+      {
+        path: "/signup",
+        element: <SignupForm />,
+      },
+    ],
+  },
+]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <RouterProvider router={router} />
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
