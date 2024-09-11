89a96bac5d49f35f46667549315f8a0f0329d131
EXCervantes <160692371+EXCervantes@users.noreply.github.com>
2024-09-11 01:03:16 -0500

    Edited Navigation

--- client/src/components/RightNav.jsx
+++ client/src/components/RightNav.jsx
@@ -1,17 +1,7 @@
 import { Button, Drawer, Sidebar, TextInput } from "flowbite-react";
 import { useState } from "react";
-import {
-  HiChartPie,
-  HiClipboard,
-  HiCollection,
-  HiInformationCircle,
-  HiLogin,
-  HiPencil,
-  HiSearch,
-  HiShoppingBag,
-  HiUsers,
-} from "react-icons/hi";
-import { FaHome, FaUser, FaCog } from "react-icons/fa";
+import { HiChartPie, HiInformationCircle, HiLogout } from "react-icons/hi";
+import { FaUser, FaCog } from "react-icons/fa";
 import { BsBoxArrowLeft } from "react-icons/bs";
 
 // TODO Fix Flowbite to work with React components
@@ -30,60 +20,39 @@ const RightNav = () => {
             pill
             onClick={() => setIsOpen(true)}
           >
-            <BsBoxArrowLeft className="h-6 w-6" />
+            <BsBoxArrowLeft className="h-5 w-5" />
           </Button>
         </div>
         <Drawer open={isOpen} onClose={handleClose} position="right">
           <Drawer.Header title="MENU" titleIcon={() => <></>} />
           <Drawer.Items>
             <Sidebar
-              aria-label="Sidebar with multi-level dropdown example"
+              aria-label="User sidebar for app functions"
               className="[&>div]:bg-transparent [&>div]:p-0"
             >
               <div className="flex h-full flex-col justify-between py-2">
                 <div>
-                  <form className="pb-3 md:hidden">
-                    <TextInput
-                      icon={HiSearch}
-                      type="search"
-                      placeholder="Search"
-                      required
-                      size={32}
-                    />
-                  </form>
                   <Sidebar.Items>
                     <Sidebar.ItemGroup>
                       <Sidebar.Item href="/" icon={HiChartPie}>
                         Dashboard
                       </Sidebar.Item>
-                      <Sidebar.Item href="/users/list" icon={HiUsers}>
+                      <Sidebar.Item href="/users/list" icon={FaUser}>
                         Users list
                       </Sidebar.Item>
                       <Sidebar.Item
                         href="/authentication/sign-in"
-                        icon={HiLogin}
+                        icon={HiLogout}
                       >
                         Sign in
-                      </Sidebar.Item>
-                      <Sidebar.Item
-                        href="/authentication/sign-up"
-                        icon={HiPencil}
-                      >
-                        Sign up
                       </Sidebar.Item>
                     </Sidebar.ItemGroup>
                     <Sidebar.ItemGroup>
                       <Sidebar.Item
-                        href="https://github.com/themesberg/flowbite-react/"
-                        icon={HiClipboard}
-                      >
-                        Docs
-                      </Sidebar.Item>
-                      <Sidebar.Item
                         href="https://flowbite-react.com/"
-                        icon={HiCollection}
+                        icon={FaCog}
                       >
-                        Components
+                        Settings
                       </Sidebar.Item>
                       <Sidebar.Item
                         href="https://github.com/themesberg/flowbite-react/issues"
